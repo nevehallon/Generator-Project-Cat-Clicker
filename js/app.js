fb1f85a450d0e89776ffcc2c44dc3af24bdcbee2
Neveh Allon <nevehallon@gmail.com>
2018-04-04 09:55:06 +0300

    Number of clicks increments when each one of the cat picture is clicked

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-let count = $(".touched"),
-  add = 0;
+const bod = $('body'),
+  arr = ["img/kitty.jpg", "img/kitty2.jpg"];
 
-$('.kitty').click(function() {
-  //the element has been clicked... do stuff here
-  add++;
-  count.text(add);
+let fun = function(img, elm, num) {
+  img.click(function() {
+    num++;
+    elm.text(num);
+  });
+};
+
+$(function() {
+  arr.forEach(function(element, index) {
+    bod.append(`<h1>Touch this Kitty!</h1>
+    <img class="kitty${index}" src=${element} alt="Cutest kitty you've ever seen">
+    <div>
+      <p>This kitty has been touched <span class="count${index}">0</span> times!</p>
+    </div>`);
+
+    this[`cat${index}`] = $(`.kitty${index}`);
+    this[`count${index}`] = $(`.count${index}`);
+    this[`add${index}`] = 0;
+
+    fun(this[`cat${index}`], this[`count${index}`], this[`add${index}`]);
+  });
 });
